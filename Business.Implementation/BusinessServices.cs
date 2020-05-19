49fa467799154a6d9adbdb568f80a689608cbf9f
Влад Никулин <vldnik0404@gmail.com>
2020-05-19 10:32:43 +0300

    add container to business

---
+++ Business.Implementation/BusinessServices.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Business.Abstraction;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.Implementation
+{
+    public static class BusinessServices
+    {
+        public static IServiceCollection RegisterDataServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDishService, DishService>();
+
+            //чтобы такое написать, нужно создать IngredientService :D
+            //services.AddTransient<IIngredientService, IngredientService>();
+
+            var mapperConfig = new MapperConfiguration(c => c.AddProfile(new AutomapperProfile()));
+
+            var mapper = mapperConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
