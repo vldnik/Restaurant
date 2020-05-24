dbb542412f98a447ea921aa8b01e4bf8fee8b42b
vlad <vlad@63B0>
2020-05-24 21:11:55 +0300

    Add IDishRepository

---
+++ Data.Abstraction/IDishRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Data.Abstraction
+{
+    public interface IDishRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> FindAll();
+
+        IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression);
+
+        void Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
